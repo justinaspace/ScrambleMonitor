501ce26033f0a2b756e04cdeeaa2a87a0fb635f9
justinaspace <justpac@gmail.com>
2026-04-28 08:55:26 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -145,7 +145,7 @@ async def check_slots():
 
         print(f"Group B is {pct_value}% filled.")
 
-        if 0 < pct_value < 100:
+        if pct_value >= 0:
             send_all(
                 f"🚨 SCRAMBLE ALERT 🚨\n\n"
                 f"Group B investment is OPEN!\n"
