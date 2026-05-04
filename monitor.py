92ea7188a10616a6511a4aeb8ac2ffe333138db2
justinaspace <justpac@gmail.com>
2026-05-04 07:35:12 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -215,7 +215,7 @@ async def check_slots():
                 logging.info("Group B is 100%% full. No alert.")
 
         except Exception as e:
-            send_all(f"⚠️ Monitor Crash: {str(e)[:200]}")
+            send_all("⚠️ Scramble Groupe B Bot.\nUpdate the Cookies.")
         finally:
             await browser.close()
 
